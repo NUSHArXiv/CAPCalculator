93a48b7429622ce732aa6fafc40b22015a31f4cc
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 01:29:32 +0800

    Update CAP Class

--- app/src/main/java/com/thepyprogrammer/capcalc/model/CAP.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/model/CAP.kt
@@ -1,4 +1,22 @@
 package com.thepyprogrammer.capcalc.model
 
-class CAP(val modules: HashMap<Module, Int>) {
+import kotlin.collections.HashMap
+
+class CAP() {
+    var modules = HashMap<Module, Double>();
+    var cap = 0.0;
+
+    constructor(modules: HashMap<Module, Double>) : this() {
+        this.modules = modules;
+        var above = 0.0
+        var below = 0.0
+        modules.forEach() {
+            below += it.key.mc
+            above += it.key.mc * it.value
+        }
+        if(above > 0) this.cap = above / below
+
+
+    }
+
 }
