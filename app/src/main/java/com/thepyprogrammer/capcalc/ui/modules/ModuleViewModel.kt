255edfd8b0fcbe807a89de6c0b32abf80437c209
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 21:24:49 +0800

    Update ModuleViewModel.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/ui/modules/ModuleViewModel.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/ui/modules/ModuleViewModel.kt
@@ -1,4 +1,13 @@
 package com.thepyprogrammer.capcalc.ui.modules
 
-class ModuleViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ModuleViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is modules Fragment"
+    }
+    val text: LiveData<String> = _text
 }
