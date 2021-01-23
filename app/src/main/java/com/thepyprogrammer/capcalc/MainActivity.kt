51a9b7a5da644749abef143ca45d5b52485e5f08
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 15:48:59 +0800

    Open database in MainActivity.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/MainActivity.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/MainActivity.kt
@@ -2,13 +2,20 @@ package com.thepyprogrammer.capcalc
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
-
+import com.thepyprogrammer.capcalc.model.Database
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var database = Database.currentOccurence
+        if(database == null) {
+            val inputStream: InputStream = resources.openRawResource(R.raw.data)
+            database = Database(inputStream)
+        }
+
     }
 }
