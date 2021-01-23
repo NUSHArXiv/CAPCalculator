028054eebe49aa809c331198676b84247ff6fd06
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 20:36:01 +0800

    Add BottomNavigationBar to app

--- app/src/main/java/com/thepyprogrammer/capcalc/MainActivity.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.thepyprogrammer.capcalc
 
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.thepyprogrammer.capcalc.model.Database
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.InputStream
@@ -16,6 +21,16 @@ class MainActivity : AppCompatActivity() {
             val inputStream: InputStream = resources.openRawResource(R.raw.data)
             database = Database(inputStream)
         }
+
+        val navView: BottomNavigationView = nav_view
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_modules))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
     }
 }
