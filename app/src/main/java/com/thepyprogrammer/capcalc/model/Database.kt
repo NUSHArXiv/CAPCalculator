ab776e01e8d2b0b23ff16851cb08373b4ec83ebb
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 15:49:36 +0800

    Update Database.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/model/Database.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/model/Database.kt
@@ -1,16 +1,21 @@
 package com.thepyprogrammer.capcalc.model
 
+import com.thepyprogrammer.capcalc.R
 import java.io.InputStream
 import java.util.*
 
 
 class Database internal constructor(moduleList: InputStream) : ArrayList<Module?>() {
+    companion object {
+        var currentOccurence: Database? = null
+    }
     init {
         val sc = Scanner(moduleList)
         while (sc.hasNext()) {
             val entry = sc.nextLine().split(",").toTypedArray()
             add(Module(entry[0].trim { it <= ' ' }, entry[1].trim { it <= ' ' }.toDouble(), entry[2].trim { it <= ' ' }, entry[3].trim { it <= ' ' }.toInt(), entry[4].trim { it <= ' ' }.toInt()))
         }
+        currentOccurence = this;
     }
 
     fun getCodes(): ArrayList<String> {
@@ -44,4 +49,5 @@ class Database internal constructor(moduleList: InputStream) : ArrayList<Module?
         }
         return arr
     }
+
 }
