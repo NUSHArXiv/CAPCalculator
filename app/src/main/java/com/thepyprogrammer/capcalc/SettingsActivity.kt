35f54f50f573f7aaea8e90eb65fd70bc6fb90e0a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 15:49:15 +0800

    Open database in SettingsActivity.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/SettingsActivity.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/SettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.thepyprogrammer.capcalc
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
-
+import com.thepyprogrammer.capcalc.model.Database
 import kotlinx.android.synthetic.main.settings_activity.*
+import java.io.InputStream
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -18,6 +21,17 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val adapter = ArrayAdapter<Double>(this, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        cap.adapter = adapter
+
+        var database = Database.currentOccurence
+        if(database == null) {
+            val inputStream: InputStream = resources.openRawResource(R.raw.data)
+            database = Database(inputStream)
+        }
+
 
     }
 
