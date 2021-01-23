27f04b47ccc19d1bbca36fa51cde4748a550860a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 12:09:51 +0800

    Add toString method to Module.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/model/Module.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/model/Module.kt
@@ -1,3 +1,7 @@
 package com.thepyprogrammer.capcalc.model
 
-data class Module(val code:String, val mc:Double, val name:String, val year:Int, val semester:Int)
+data class Module(val code:String, val mc:Double, val name:String, val year:Int, val semester:Int) {
+    override fun toString(): String {
+        return "$code $name (mc=$mc)"
+    }
+}
