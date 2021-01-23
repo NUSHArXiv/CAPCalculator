bc187c531e96676de1ad043d23d64d1fffb7287e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 21:24:45 +0800

    Update ModulesFragment.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/ui/modules/ModulesFragment.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/ui/modules/ModulesFragment.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.thepyprogrammer.capcalc.R
+import com.thepyprogrammer.capcalc.ui.home.HomeViewModel
 
-class ModulesFragment : Fragment() {
+class HomeFragment : Fragment() {
+
+    private lateinit var moduleViewModel: ModuleViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_modules, container, false)
+        moduleViewModel =
+            ViewModelProvider(this).get(ModuleViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_modules, container, false)
+        val textView: TextView = root.findViewById(R.id.text_modules)
+        moduleViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
     }
 }
