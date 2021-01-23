90fc1879c20a9fc8d8ef973a3fb971647097ff42
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-23 15:07:17 +0800

    Update MainActivity.kt

--- app/src/main/java/com/thepyprogrammer/capcalc/MainActivity.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/MainActivity.kt
@@ -4,23 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
+        setContentView(R.layout.activity_main)
     }
 }
