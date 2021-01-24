aabbcbbeb44d17ff07373e7c245911fe18f8ce6a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-24 11:42:59 +0800

    Add module to ModuleViewModel

--- app/src/main/java/com/thepyprogrammer/capcalc/ui/modules/ModuleViewModel.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/ui/modules/ModuleViewModel.kt
@@ -3,6 +3,12 @@ package com.thepyprogrammer.capcalc.ui.modules
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.thepyprogrammer.capcalc.model.Module
 
 class ModuleViewModel : ViewModel() {
+
+    private val _module = MutableLiveData<Module>().apply {
+        value = null
+    }
+    val module: LiveData<Module> = _module
 }
