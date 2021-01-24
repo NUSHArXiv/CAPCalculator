47783bf48b5521e979d1bf0c33f0a64ff6d7d207
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-24 14:56:40 +0800

    Create Cell.kt

---
+++ app/src/main/java/com/thepyprogrammer/capcalc/model/table/Cell.kt
@@ -0,0 +1,10 @@
+package com.thepyprogrammer.capcalc.model.table
+
+import androidx.annotation.Nullable
+
+open class Cell(
+        @field:Nullable
+        @get:Nullable
+        @param:Nullable
+        val data: Any
+)
