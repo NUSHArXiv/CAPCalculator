b339cfbf1ccec56ddf9d12a19dafa6bc2dfbf69f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-29 23:04:55 +0800

    Rename Database to ModuleDatabase

--- app/src/main/java/com/thepyprogrammer/capcalc/model/ModuleDatabase.kt
+++ app/src/main/java/com/thepyprogrammer/capcalc/model/ModuleDatabase.kt
@@ -1,14 +1,13 @@
 package com.thepyprogrammer.capcalc.model
 
-import android.graphics.PointF.length
-import com.thepyprogrammer.capcalc.R
+import com.thepyprogrammer.capcalc.model.data.Module
 import java.io.InputStream
 import java.util.*
 
 
-class Database internal constructor(moduleList: InputStream) : ArrayList<Module?>() {
+class ModuleDatabase internal constructor(moduleList: InputStream) : ArrayList<Module?>() {
     companion object {
-        var currentOccurence: Database? = null
+        var currentOccurence: ModuleDatabase? = null
     }
 
     val modules = HashMap<Module, Double>()
@@ -18,14 +17,22 @@ class Database internal constructor(moduleList: InputStream) : ArrayList<Module?
         sc.nextLine()
         while (sc.hasNext()) {
             val entry = sc.nextLine().split(",").toTypedArray()
-            add(Module(entry[0].trim { it <= ' ' }, entry[1].trim { it <= ' ' }.toDouble(), entry[2].trim { it <= ' ' }, entry[3].trim { it <= ' ' }.toInt(), entry[4].trim { it <= ' ' }.toInt()))
+            add(
+                Module(
+                    entry[0].trim { it <= ' ' },
+                    entry[1].trim { it <= ' ' }.toDouble(),
+                    entry[2].trim { it <= ' ' },
+                    entry[3].trim { it <= ' ' }.toInt(),
+                    entry[4].trim { it <= ' ' }.toInt()
+                )
+            )
         }
-        currentOccurence = this;
+        currentOccurence = this
     }
 
     fun getCodes(): ArrayList<String> {
         val arr = ArrayList<String>()
-        for(module in this) {
+        for (module in this) {
             if (module != null) arr.add(module.code)
         }
         return arr
@@ -33,7 +40,7 @@ class Database internal constructor(moduleList: InputStream) : ArrayList<Module?
 
     fun getNames(): ArrayList<String> {
         val arr = ArrayList<String>()
-        for(module in this) {
+        for (module in this) {
             if (module != null) arr.add(module.name)
         }
         return arr
@@ -41,7 +48,7 @@ class Database internal constructor(moduleList: InputStream) : ArrayList<Module?
 
     fun getMCs(): ArrayList<Double> {
         val arr = ArrayList<Double>()
-        for(module in this) {
+        for (module in this) {
             if (module != null) arr.add(module.mc)
         }
         return arr
@@ -49,7 +56,7 @@ class Database internal constructor(moduleList: InputStream) : ArrayList<Module?
 
     fun getFullNames(): ArrayList<String> {
         val arr = ArrayList<String>()
-        for(module in this) {
+        for (module in this) {
             if (module != null) arr.add("%s (%s)".format(module.code, module.name))
         }
         return arr
@@ -58,15 +65,16 @@ class Database internal constructor(moduleList: InputStream) : ArrayList<Module?
     fun getStrings(): ArrayList<String> {
         val arr = ArrayList<String>()
         this.forEach {
-            if(it != null) arr.add(arr.toString());
+            if (it != null) arr.add(arr.toString())
         }
         return arr
     }
 
     fun getModuleByFullName(fullName: String): Module? {
-        val arr = getFullNames();
-        for(i in 0..arr.size) {
-            if(arr[i] == fullName) return this[i]
+        if (fullName.isEmpty()) return null
+        val arr = getFullNames()
+        for (i in 0..arr.size) {
+            if (arr[i] == fullName) return this[i]
         }
         return null
     }
